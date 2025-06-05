0e30e18536d046dadccd1f5487dd6c28882bec01
Zubair Umatiya <zubair.umatiya@gmail.com>
2025-06-05 18:03:28 -0500

    added crude unusable, infant backend

---
+++ backend/routes/vacationRoutes.ts
@@ -0,0 +1,5 @@
+import express from "express";
+const router = express.Router();
+import db from "../db/db.js";
+
+export default router;
---
+++ backend/server.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import { Request, Response } from "express";
+const app = express();
+import vacationRoutes from "./routes/vacationRoutes.js";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+
+dotenv.config();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+
+app.use("/", vacationRoutes);
+
+const port = process.env.PORT;
+
+app.use(function (req, res, next) {
+  const err = new Error("Not Found") as Error & { status?: number };
+  err.status = 404;
+  next(err);
+});
+
+if (app.get("env") === "development") {
+  app.use(function (
+    err: Error & { status?: number },
+    req: Request,
+    res: Response
+  ) {
+    res.status(err.status || 500).json({ message: err.message, error: err });
+  });
+}
