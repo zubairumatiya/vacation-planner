17baa5a28a5d244d033c77cc124110baf09f5a8a
Zubair Umatiya <zubair.umatiya@gmail.com>
2026-02-17 19:10:07 -0600

    updated relative frontend vite config paths to absolute

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,14 +3,22 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     https: {
-      key: fs.readFileSync(path.resolve("../backend/certs/localhost-key.pem")),
-      cert: fs.readFileSync(path.resolve("../backend/certs/localhost.pem")),
+      key: fs.readFileSync(
+        path.resolve(__dirname, "../backend/certs/localhost-key.pem"),
+      ),
+      cert: fs.readFileSync(
+        path.resolve(__dirname, "../backend/certs/localhost.pem"),
+      ),
     },
     port: Number(process.env.PORT || "5173"),
   },
