34fcb559807b9e26ba1ad53d37ade73a4a085983
Zubair Umatiya <zubair.umatiya@gmail.com>
2026-03-10 14:02:44 -0500

    addded logo to simple header

--- frontend/src/pages/SimpleHeader.tsx
+++ frontend/src/pages/SimpleHeader.tsx
@@ -1,12 +1,16 @@
-import placeholderLogo from "../assets/icons/react.svg";
+import vacationPlannerLogo from "../assets/icons/vacation-planner.svg";
 import { Link } from "react-router-dom";
 import styles from "../styles/SimpleHeader.module.css";
 
 const SimpleHeader = () => {
   return (
     <header className={styles.header}>
-      <Link to="/">
-        <img src={placeholderLogo} alt="Vacation-Planner-Logo" />
+      <Link to="/" className={styles.logoLink}>
+        <img
+          src={vacationPlannerLogo}
+          alt="Vacation Planner"
+          className={styles.logo}
+        />
       </Link>
     </header>
   );
